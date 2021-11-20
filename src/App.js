f0e89f23d408473c614a73dad986ba0837c78814
GovindBisen <govind.bisen@gmail.com>
2021-11-20 23:06:36 +0530

    todo till add item

--- src/App.js
+++ src/App.js
@@ -1,24 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import React from "react";
+import { useState } from "react";
 
 function App() {
+  const [todo, setTodo] = useState("");
+  const [todolist, setTodoList] = useState([]);
+  const addList = () => setTodoList([...todolist, todo]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="App">
+        <header className="App-header">
+          <b>TODO</b>
+        </header>
+        <div>
+          <input
+            type="text"
+            className="Todo"
+            id="inp"
+            onChange={(event) => setTodo(event.target.value)}
+          />
+          <button onClick={addList}>Add Task</button>
+          {console.log(todolist)}
+          <div className="tasks">
+            <ul>
+              {todolist.map((t) => (
+                <li key={Math.random()}>{t}</li>
+              ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </>
   );
 }
 
